bcfbe36dde759b138a58e8edf630513f3f6e2952
KarwaiKang <kkang9093@bths.edu>
2018-09-20 19:11:03 -0400

    Completed most string methods

--- src/KKang/KKangLib.java
+++ src/KKang/KKangLib.java
@@ -7,6 +7,42 @@ public class KKangLib {
                 return false;
             }
         }
+
         return true;
     }
+
+    public static String dateStr(String str) {
+        return str.substring(3,5) + " - " + str.substring(0,2) + " - " + str.substring(6);
+    }
+
+    public static String cutOut(String mainStr, String subStr) {
+        int index = mainStr.indexOf(subStr);
+        String result = mainStr;
+
+        if (index != -1) {
+            result = mainStr.substring(0, index);
+
+            if (index != mainStr.length()-3)
+                result += mainStr.substring(index+subStr.length());
+        }
+
+        return result;
+    }
+
+    public static void fooBarBaz(int num1) {
+        for (int i = 1; i <= num1; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("baz ");
+            else if (i % 3 == 0)
+                System.out.print("foo ");
+            else if (i % 5 == 0)
+                System.out.print("bar ");
+            else
+                System.out.print(i + " ");
+        }
+    }
+
+    // Math Methods
+
+
 }
--- src/KKang/Main.java
+++ src/KKang/Main.java
@@ -5,5 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println(KKangLib.isPalindrome("racecar"));
         System.out.println(KKangLib.isPalindrome("eclair"));
+
+        System.out.println(KKangLib.dateStr("01/08/1999"));
+
+        System.out.println(KKangLib.cutOut("catatonic cat", "cat"));
+        System.out.println(KKangLib.cutOut("atomic cat", "cat"));
+
+        KKangLib.fooBarBaz(15);
     }
 }
