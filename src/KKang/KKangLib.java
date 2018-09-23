6d939d4ca348d42a4c6acbac8b40654113090708
KarwaiKang <kkang9093@bths.edu>
2018-09-23 11:53:50 -0400

    Added vigCipher method Added stringUnion method Added leastCommonMultiple method Added more tests Added comments explaining more complicated methods Improved readability of testFile output Fixed cutOut method for subStr of variable length

--- src/KKang/KKangLib.java
+++ src/KKang/KKangLib.java
@@ -24,7 +24,7 @@ public class KKangLib {
         if (i != -1) {
             result = mainStr.substring(0, i);
 
-            if (i != mainStr.length() - 3)
+            if (i != mainStr.length() - subStr.length())
                 result += mainStr.substring(i + subStr.length());
         }
 
@@ -34,7 +34,7 @@ public class KKangLib {
     public static void fooBarBaz(int num1) {
         for (int i = 1; i <= num1; i++) {
             if (i % 3 == 0 && i % 5 == 0)
-                System.out.println("baz ");
+                System.out.print("baz ");
             else if (i % 3 == 0)
                 System.out.print("foo ");
             else if (i % 5 == 0)
@@ -42,6 +42,52 @@ public class KKangLib {
             else
                 System.out.print(i + " ");
         }
+        System.out.println();
+    }
+
+    /*
+        The way this works is that it utilizes the ability of characters to be represented by integers as defined in ASCII.
+        To cover cases where the key is shorter than the message, modulo of the length of the key ensures no "out of bounds" errors.
+        For each character of the message and the corresponding key character, the integer value of the character 'A' is subtracted.
+        This lets the value of 'A' be 0 and 'Z' be 25. This is necessary so that a modulo of 26 can be used to roll over 'Z' to 'A'.
+        The value of 'A' is then added again so the characters have the correct ASCII value.
+        Finally, the 'int' is casted back to a 'char' and concatenated to a new string 'result'.
+
+        CAVEAT: This uses String.charAt().
+        CAVEAT: This only works in all caps.
+    */
+
+    public static String vigCipher(String message, String key) {
+        String result = "";
+
+        for (int i = 0; i < message.length(); i++)
+            result += (char) ((((message.charAt(i) - 'A') + (key.charAt(i % key.length()) - 'A')) % 26) + 'A');
+
+        return result;
+    }
+
+    public static int stringUnion(String word1, String word2, String word3) {
+        String lettersShared = "";
+
+        for (int i = 0; i < word1.length(); i++) {
+            String letter = word1.substring(i, i + 1);
+            if ((word2.indexOf(letter) != -1 || word3.indexOf(letter) != -1) && lettersShared.indexOf(letter) == -1)
+                lettersShared += letter;
+        }
+
+        for (int i = 0; i < word2.length(); i++) {
+            String letter = word2.substring(i, i + 1);
+            if ((word1.indexOf(letter) != -1 || word3.indexOf(letter) != -1) && lettersShared.indexOf(letter) == -1)
+                lettersShared += letter;
+        }
+
+        for (int i = 0; i < word3.length(); i++) {
+            String letter = word3.substring(i, i + 1);
+            if ((word1.indexOf(letter) != -1 || word2.indexOf(letter) != -1) && lettersShared.indexOf(letter) == -1)
+                lettersShared += letter;
+        }
+
+        return lettersShared.length();
     }
 
     // Math Methods
@@ -68,9 +114,17 @@ public class KKangLib {
             System.out.print(base * i + " ");
     }
 
+    // Utilizes the summation formula.
+
     public static int sumUpTo(int n) {
         return (n * (n + 1)) / 2;
     }
+
+    /*
+        Checks if a number n has the factors 2 to the square root of n. If so, it is not prime.
+
+        CAVEAT: Uses Math.sqrt().
+    */
 
     public static void primePrinter(int num) {
         int primes = 0;
@@ -92,6 +146,8 @@ public class KKangLib {
         }
     }
 
+    // CAVEAT: Uses Math.sqrt().
+
     public static String quadSolver(double a, double b, double c) {
         double plusRoot = (-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
         double minusRoot = (-b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
@@ -100,5 +156,62 @@ public class KKangLib {
             return "imaginary.";
 
         return String.valueOf(plusRoot + ", " + minusRoot);
+    }
+
+    /*
+        This method is probably not the cleanest or most efficient at all.
+        First, the method finds the LCM of 'num1' and 'num2', and then the LCM of that and 'num3'.
+        I still don't know if this way is full proof.
+
+        Instead of adding the multiple of the smaller number to itself,
+        I add the ratio of the larger to the smaller times the multiple.
+        We can tell which number is smaller or larger based on the ratio. (If a / b < 1, a < b)
+        In theory, this should be faster in certain cases where the the ratio is 2 or greater.
+
+        For example, 5 vs 51.
+        In the old way, 5 would be added to 10 times (+5 +5 +5 +5 +5 +5 +5 +5 +5 +5) before it was larger.
+        In the new way, 5 would be added to only 1 time (+(5 * (51 / 5)), or +50).
+    */
+
+    public static int leastCommonMultiple(int num1, int num2, int num3) {
+        int num1Mult = num1;
+        int num2Mult = num2;
+        int num3Mult = num3;
+
+        while (num1 != num2) {
+            if (num2 % num1 == 0) {
+                num1 = num2;
+                break;
+            }
+            if (num1 % num2 == 0)
+                break;
+
+            int ratio = num1 / num2;
+
+            if (ratio == 0)
+                num1 += num2 / num1 * num1Mult;
+            else
+                num2 += ratio * num2Mult;
+        }
+
+        num1Mult = num1;
+
+        while (num1 != num3) {
+            if (num3 % num1 == 0) {
+                num1 = num3;
+                break;
+            }
+            if (num1 % num3 == 0)
+                break;
+
+            int ratio = num1 / num3;
+
+            if (ratio == 0)
+                num1 += num3 / num1 * num1Mult;
+            else
+                num3 += ratio * num3Mult;
+        }
+
+        return num1;
     }
 }
--- src/KKang/testFile.java
+++ src/KKang/testFile.java
@@ -3,41 +3,94 @@ package KKang;
 public class testFile {
 
     public static void main(String[] args) {
-	    System.out.println(KKangLib.isPalindrome("racecar"));
-        System.out.println(KKangLib.isPalindrome("amanaplanacanalpanama"));
-        System.out.println(KKangLib.isPalindrome("eclair"));
+	    System.out.println("\'racecar\' is a palindrome. " + KKangLib.isPalindrome("racecar"));
+        System.out.println("\'amanaplanacanalpanama\' is a palindrome. " + KKangLib.isPalindrome(""));
+        System.out.println("\'eclair\' is a palindrome. " + KKangLib.isPalindrome("eclair"));
 
-        System.out.println(KKangLib.dateStr("01/08/1999"));
+        System.out.println();
 
-        System.out.println(KKangLib.cutOut("catatonic cat", "cat"));
-        System.out.println(KKangLib.cutOut("atonic cat", "cat"));
-        System.out.println(KKangLib.cutOut("dog", "cat"));
+        System.out.println("\"01/08/1999\" becomes " + KKangLib.dateStr("01/08/1999"));
+        System.out.println("\"07/03/2001\" becomes " + KKangLib.dateStr("07/03/2001"));
+        System.out.println("\"04/07/1776\" becomes " + KKangLib.dateStr("04/07/1776"));
 
+        System.out.println();
+
+        System.out.println("\"catatonic cat\" without \"cat\" is " + KKangLib.cutOut("catatonic cat", "cat"));
+        System.out.println("\"atonic cat\" without \"cat\" is " + KKangLib.cutOut("atonic cat", "cat"));
+        System.out.println("\"dog\" without \"cat\" is " + KKangLib.cutOut("dog", "cat"));
+        System.out.println("\"conch shell\" without \"shell\" is " + KKangLib.cutOut("conch shell", "shell"));
+
+        System.out.println();
+
+        KKangLib.fooBarBaz(3);
         KKangLib.fooBarBaz(15);
+        KKangLib.fooBarBaz(45);
 
-        System.out.println(KKangLib.isFibonacci(5));
-        System.out.println(KKangLib.isFibonacci(55));
-        System.out.println(KKangLib.isFibonacci(144));
-        System.out.println(KKangLib.isFibonacci(10));
+        System.out.println();
 
+        System.out.println(KKangLib.vigCipher("ATTACKATDAWN", "LEMON"));
+        System.out.println(KKangLib.vigCipher("CALLOUTTHESTORM", "PICKLE"));
+        System.out.println(KKangLib.vigCipher("THEREARENOBUSESINGENSOKYO", "THEREARE"));
+
+        System.out.println();
+
+        System.out.println("The words \"hello\", \"hola\", and \"nihao\" share " + KKangLib.stringUnion("hello", "hola", "nihao") + " unique letters between at least two of them.");
+        System.out.println("The words \"ed\", \"edd\", n \"eddy\" share " + KKangLib.stringUnion("ed", "edd", "eddy") + " unique letters between at least two of them.");
+        System.out.println("The words \"red\", \"white\", and \"blue\" share " + KKangLib.stringUnion("red", "white", "blue") + " unique letter between at least two of them.");
+        System.out.println("The words \"seven\", \"eight\", and \"nine\" share " + KKangLib.stringUnion("seven", "eight", "nine") + " unique letters between at least two of them.");
+        System.out.println("The words \"typewriter\", \"alfalfa\", and \"zzz\" share " + KKangLib.stringUnion("typewriter", "alfalfa", "zzz") + " unique letters between at least two of them.");
+
+        System.out.println();
+
+        System.out.println("5 is a fibonacci number. " + KKangLib.isFibonacci(5));
+        System.out.println("55 is a fibonacci number. " + KKangLib.isFibonacci(55));
+        System.out.println("144 is a fibonacci number. " + KKangLib.isFibonacci(144));
+        System.out.println("10 is a fibonacci number. " + KKangLib.isFibonacci(10));
+
+        System.out.println();
+
+        System.out.print("The products of 5 × n where n equals all integers from 0 to 4: ");
         KKangLib.multiplicationTable(5,4);
         System.out.println();
+        System.out.print("The products of 12 × n where n equals all integers from 0 to 10: ");
         KKangLib.multiplicationTable(12,10);
         System.out.println();
+        System.out.print("The products of 7 × n where n equals all integers from 0 to 8: ");
         KKangLib.multiplicationTable(7,8);
         System.out.println();
 
-        System.out.println(KKangLib.sumUpTo(5));
-        System.out.println(KKangLib.sumUpTo(8));
-        System.out.println(KKangLib.sumUpTo(58));
+        System.out.println();
 
+        System.out.println("The sum of integers from 1 to 5 is " + KKangLib.sumUpTo(5));
+        System.out.println("The sum of integers from 1 to 8 is " + KKangLib.sumUpTo(8));
+        System.out.println("The sum of integers from 1 to 58 is " + KKangLib.sumUpTo(58));
+
+        System.out.println();
+
+        System.out.print("The first prime number is: ");
+        KKangLib.primePrinter(1);
+        System.out.println();
+        System.out.print("The first 5 prime numbers are: ");
+        KKangLib.primePrinter(5);
+        System.out.println();
+        System.out.print("The first 25 prime numbers are: ");
         KKangLib.primePrinter(25);
         System.out.println();
 
-        System.out.println("The roots of x^2 are " + KKangLib.quadSolver(1, 0, 0));
-        System.out.println("The roots of x^2 + x are " + KKangLib.quadSolver(1, 1, 0));
-        System.out.println("The roots of -x^2 + 2x + 3 are " + KKangLib.quadSolver(-1, 2, 3));
-        System.out.println("The roots of x^2 + 5 are " + KKangLib.quadSolver(1, 0, 5));
-        System.out.println("The roots of 0.5x^2 - 0.83x - 0.725 are " + KKangLib.quadSolver(.5, -.83, -.725));
+        System.out.println();
+
+        System.out.println("The roots of x² are " + KKangLib.quadSolver(1, 0, 0));
+        System.out.println("The roots of x² + x are " + KKangLib.quadSolver(1, 1, 0));
+        System.out.println("The roots of -x² + 2x + 3 are " + KKangLib.quadSolver(-1, 2, 3));
+        System.out.println("The roots of x² + 5 are " + KKangLib.quadSolver(1, 0, 5));
+        System.out.println("The roots of 0.5x² - 0.83x - 0.725 are " + KKangLib.quadSolver(.5, -.83, -.725));
+
+        System.out.println();
+
+        System.out.println("The LCM of 3, 4, and 5 is " + KKangLib.leastCommonMultiple(3, 4, 5));
+        System.out.println("The LCM of 9, 8, and 7 is " + KKangLib.leastCommonMultiple(9, 8, 7));
+        System.out.println("The LCM of 9, 12, and 3 is " + KKangLib.leastCommonMultiple(9, 12, 3));
+        System.out.println("The LCM of 5, 50, and 500 is " + KKangLib.leastCommonMultiple(5, 50, 500));
+        System.out.println("The LCM of 4, 4, and 4 is " + KKangLib.leastCommonMultiple(4, 4, 4));
     }
 }
